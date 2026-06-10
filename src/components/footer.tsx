7366a078472a0e6994106039a7c553c4f84659a5
zubeyranwar <zubeyranwar@gmail.com>
2026-06-10 12:23:04 +0300

    chore: use gebeta map instead of google

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -28,12 +28,13 @@ export const Footer = () => {
 
                         <div className="col-span-2 pr-4">
                             <iframe
-                                className="w-full h-150"
-                                frameBorder="0"
-                                style={{ border: 0 }}
-                                allowFullScreen
-                                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBVizdQeh3udy11xDc5Ao2YStR2gLc-rfc&q=XQPP%2BRW%2C%20Addis%20Ababa%201000&maptype=roadmap&zoom=14"
-                            />
+                                src="https://gebeta.app/embed/map?t=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..tlV6YPdZRUsKy0zY.5y4k8rE4PbuN-i3hJQUlZL7MDVFfosskFlpB9kxgaUDOYDVg1HsndYQTbskjQy4rzF45OoDtZ9GFDDa-nzWhawmrKDI-iLudVEd-wqyNhT5UvUKw9aO70lC9W_vSUp3GB74bKI-0TOYO9lQ82_YaL2l6k6SC9Sbn93le_LzhvfDSqGhRU2RBao__N4IrQ6EV2ILPYPJeGGDqte815AWMePikoGcK86Ou_7jLSwn5IcNli80aB2-Bd4ulT5yC4kKn9L_1RcD40YI2Y2vtiFf2G8nuHzUhmMGsG4wxMAo7kupBt10BNGrtUa_dbFdFzgZRDXb7ekbJZ8Sbyxo2ZD_XvEYn1GK2Wr_vjUOHjDT5H6fKfnw82STBFvytsCuHY5CbZTFr6FiRlFVLDdu4tAnOm-iMOLeHbzohGEIi_jRgRm690_1KAqbs7v7VZAYHKM9ph_Ex9pE6nIDPXg8_Y2vX84gwJUrEcGSPVhIXhgoV9pPdR19eQVw5O_k0fPIJVCpHUyYdKKPNu2PTXEApePO-tYlRT_DPNugjza3I8nh2PFlWpaSLW246dJaz_DWz9X6GYEV63nW4SYmAZGWOiGFncy5BXs3A5NNOnc3f06_GawXZyCVuZnQ-rMpsFX1SKuYjCkuK1SZILiZBNaBYfIl0WvseyBv6xIUdcMcGqYA16NeQ3YdkMEJiplQ2FKDmLxgrYxU_0pZxHozf7tqKiU0zC_DphUUj-JmIVhS07mZTwOG5fdiMeafIpfBpnanyFiDzU9xu6ocdfWeTiXL6SJemMLcBd-Xv2q-ctenC9_OUsjjZDf96h-Is6PVgiUy9fxYS3HHK1SoaGa8NrzZlFmBlWnDAAnBCswSG8LLQVOycasNGtAhv9w6QyCMJHlmiGJGvqbiMIUPKykcEL1scBNyDzjefI74D1AxQ0A7SW1frX3xN7J_pDFB6ZwjxR8Dh3nIio5RdV7SOw9ZGDdEGy-V-CA5bfxCm4LxMZMw654xGb8PicnLtpAm3qJF9FO3XjGIQoVIhyEXxi6itobOf2vx5W-5h8nXt31Rzp9OhX65-GrMm3RcsMb7M6Ju9Ior1s73jZ5YeNhGMTWqizbtOhut667DnOuUEXPn3SsDEsl3jgTt4eQEPFoTA_DKtDHaWj5hRPBJk9cvqmflBpjEpdirSKpYtEuI3Owt5esWIvu9MxgJ9XETQDrU5yUJttBQGOX6EXi_-0LxAmp0iA61Rj7J8DH0ctvu5c9CCR8swpB4vXNSZ8zqNBahpAYFfLsUZeT9V1hyZh8uChT0SouzHsXRkVLHMBa7QuYP6W1mJRc1cc24h0fDA4-DMRHdtBH1lpZ6XoPh9tH3ldG28JH9zPUotssnqYRsO4T4X3GFMEiF8EDSMjPOujKSWewJ3YmTbDRrrwWnsBFdVWlbZ0dlEWgYVBXq54paSCMCUUD-FBab12CfF2Y0N71uA3eKtZ_w5vfGUzB1CylOcxtgW.ZVBCNIrav1eK-JMgLjX4eA"
+                                width="600"
+                                height="450"
+                                // style="border:0;border-radius:8px"
+                                // allowfullscreen
+                                loading="lazy"
+                            ></iframe>
                         </div>
 
                         <div className="pl-0 md:pl-4 flex flex-col gap-7.5">
