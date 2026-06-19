16b9f7b37e79bd0dff68d8bd67e00ee4308dccd5
zubeyranwar <zubeyranwar@gmail.com>
2026-06-19 15:45:19 +0300

    chore: update the map iframe

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,7 @@ import { Container } from "./container"
 import { Logo } from "./logo"
 import Link from "next/link"
 import { LayerInViewAnim } from "./layer-in-view-anim"
+// import { CodeBy } from "./code-by"
 
 export const Footer = () => {
     return (
@@ -28,7 +29,7 @@ export const Footer = () => {
 
                         <div className="col-span-2 pr-4">
                             <iframe
-                                src="https://gebeta.app/embed/map?t=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..PF-1IRpEOBKR1OOX.8TJy-UwaUiOdgKUffHVOG1XmhTb1KB3T6nBhSiDIKwqa0wfxoWkQ-HAczDNfiwEE2kR5fvU94WGEkQg7TbrSR9Vu_mI2T9i4U-ieW8YQaIlntrqOxCyIAPuHoj3DO55MBOC2mBMKH4Sjm8XFxyKWHfvg76IjR9GEfeZEi_qZWIR6iiMBf53iD7DsX9GMt2Awg1SCGSMZn0MDb1BCIK_R5D9V7SdSsLMg3BuPcZAzPa2105HHVv093zhZp8-vSGbdLgCwgZ-yK-dHjcdw-nw5__jYih4BhPsy34UH3kJ0fUfJFXhP6IGTYaLzk71Yxw3TJsecKPlDtUqNssnCZhk_6zwOoA5wa0puHWnM7aMESc_r9y831DCq3cNG4fVxKcnZqrYn9sgh_huuCYeEJFbRrufTvn0fqwAMLhzRb2IzPSnU9BCS1QjWsQFwenUWJvsCrGRW3HORIxqwDezZdY_scRZWOjldOnf8q8Smolnw3AW-dZg8NL3JeNTJ7KfqJXaK-RSGv-wy4d_KZj65iZsepB9CTHMOp-emzjoZo6OtVisYyTG62tWUAxJkP31PlnhAlqZeoxFIP48VrG1XERZefiaRS7Py2OpVM_hoO-msSsTDbKgSAe1bfVSk1f6nfuN3Yyg0wCEvVbaE6oWJiM7PvFlQ3lqtVD9a33mVsttTQYMxXrjtflgYL3jeLQ6KdoAYZxOnENFbILQ1IT_c2IDQtcmc-Av3qmeWZAJF8vUlWQe0aXlf0NR_E1e3Usvuunt18SBtRPngdwBjRK7A_maiMp3E3fHhmSsWYzhS0wL3c79Ukc339ECPpZvjPROgXYMgpXv8hcd-r3dxDuTc4v4xtrNkh5YmJlDyE_p9vVWlbDMgSoPz_ZKFrrQ6CH98Egf4Fo5008Jclz8WqFM2eRAlHikEF3Vmf0R8dQAkpD2dfDoYKfJzOdGiseiaLQb7Ih8S6atUfxZtN3qUqURsA28YWd8EqL4Dr2NI_cwjowGqnOkC63GJGXdA2-_WZUQx3BJ-5XH2UcxC6ZUCVhDwHkaBXAB-5Qx3kxTX25nzbM7UJqJ9GDkX3IhgPPfPViJmwySA-B4kqLRKuX-g9-4wrFk7dgUrVLK0KE0bKoRTQkH1wB1KRBoXzx6Nn8SPUu1EFnVNIcc3F5EkmNdmDqj2fKo-AmQbwneyjftFIjLb3xgYr_LBpg2k52aNtm8UWkkdaFOQ_pp8BZmp5wPCEdq4p1gzaVY-qUBc_utCJpCBN2NGA45XDpnf_IXT8abmxEB6l2-RTp45yJSGpbrwV3XTW1kdzWnSzVn4ZeQhvtbzpm96hbvwRfnBtp93Refqr5RyKtKpcLIXV7i5WrlEJ5i7pk_mi3nrLXA6JA2OnzGXzwYym7yPidkQCg-6T60i65wWJUu8uG3wtlK5LZZ0-LfmmdYjL-8Eds-UwsHW_b5dHZsXPNmXuvWFna3Z_gn06OGqGH9BaQnJSD5ZfjpihnncRQxWH4E_gRgBntUN4jLyP_TnDKguMBVmwoW0bFH7_EbaHbmoDTH7OiEx_8f2D-f5VoeD5tFUpT2tP1Le45CsKV7Q.-ViPnxZGVg2nJfGvrMdYMA"
+                                src="https://gebeta.app/embed/map?t=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..jEY4ULafnQ4zlKRJ.8pMDtHmh1KcqMTlV8h5NEzNjkfY-undtF_qoru8Q9pOWt5m9_YmAIItT-rhvGhTSGXisMrYGs3WCrMs0XaAljLNVP--5QIMQZhoSEtZRY7JJtXtVj4LWsfZMdm-hVxMUURr5Bv9QBpObxYD1nvw6QnAiLPviQrzpscRPXugW9G36lgkaRkiz9IsVMP9BEazRmwfan-ed6LBguXm-idF0Ird-Va4FQgqStfhOoJ5UcRky0dmRv3SVCCitFwLk-G3JUkNlkvggTqf171do5CcAZBAs3j75yLrtHeTGPqi5RlwjnZRSlS4ZG0rR_C0YzkRfCOm8Bn0qJilx4gC47Jd0GS1Upv3PdEiFWOPsZZeZSY4X0GGKuMKbhb6kMPNVUzoad1F7qgOb1TuVTfwDe3LZ0mCdMppNgsLjrEkJBtW5cBIPclmhz8UaaUNzxbt1GpQny6HJkkFQ_2Tdu3YuiZY1T5v3_LIfxDyZJSnwt5x81Uk8l5Kff6j8nX1exHxR-ip5kSfd2hBv1llZyaqzNLTVQREKi1dm4mn3QHvWY1p4v-1SlXo3VUEsgWBOM0QqcZbFu2r_djMrcYhGppdU8GsWPMnlwEpT2r3pYhuO9p6Q3FoNA9V3-S2Wt7D_Upn-RdLdvn-6dAa7V07TmUnDOZDWkmdCUsdm3Idr1m1yDGXDouO7dLdqEyElhZPaAk_rj5zPEsxI70Mv9LnnHgM5cC6Tv1g8yuLJMRbhXkXPPBx4A9V3Uh6paX78-VcHmHvSU-reXt6JaZQHcuge4LTGn8c-StNyPYt4MDxvp1tby41XvqEAdztoGm8UL5tkO9RyzxnRl_m-3071xv0a1_6G5x8y8PDMoHOXlDbNVkEEpLFoPhj_PRJxfPOoV1G6I5oK7lVhK_F76sri2sJwGy2ccZzrQKe7jLTc0Q0JVrUlbWQSrLNcybaI2XeFUGQGYcusZvLQJaiEyxkh-gRYgEwDUeKvhsT09ZZx10FfUfI84_RW4xTGLsmoLApn18EAJa-c8rhE5Pc17AQkMQCqfk3vGqbLuMLG3hJNEMvVKRTuQRJU9OJVxoaRZhCQujXMV7ZvECJHfYOwfK92Fuw7BSJ5cOpEpLqaZZ-TlODgptjvtDNYVmU8hjwk_hEKJyuhKQQNVxfhHOldHksGF7fv9wmJbZpAbs7onkng7E1xFdroWiORluAdzZAOCuZXNCL0M2yFOhlqpMC4TLepZP32A6K7SUeVsUciHVwc3xcdVBWrnB32E_op6PcDCi7rrXwh5HYDQ6Kte7Y7GjItaiNsdiu5ouwdFixwaJhcsdwnmlfal46l4SWZJvV6kF9OjbQdGQiS-53ysiXXVWmnCC1bIA520RA7g-7Tw4UPbiZUTM1_6JcPpTepr4p23GrCOB36fKPwlxMbbNsppvvYmttq2RsFeIPXZ4g9YrP2cCP0VmEGBitCaEGCQZYbrIpFOYTix50lh0xJCA94E1ZrLpBK6SHNmYlmKyytH8NlI6uqwPA.-NseBKFAtWfzkiEBxnSo_w"
                                 width="600"
                                 height="450"
                                 loading="lazy"
@@ -91,8 +92,10 @@ export const Footer = () => {
                         </div>
                     </div>
 
-                    <p className="body-normal text-gray! mt-4 md:mt-16"> © 2026 Buldak</p>
-
+                    <div className="mt-4 md:mt-16 flex justify-between items-center">
+                        {/* <CodeBy /> */}
+                        <p className="text-gray!">© 2026 Buldak</p>
+                    </div>
                 </LayerInViewAnim>
             </Container >
         </footer >
